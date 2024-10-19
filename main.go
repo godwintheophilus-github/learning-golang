21995191a80d249df19192b4fff18ba451f2f039
gdwntheophilus <85812329+gdwntheophilus@users.noreply.github.com>
2024-10-19 23:03:37 +0530

    finishing off with day 1

--- main.go
+++ main.go
@@ -1,24 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	fmt.Println("Welcome to Golang Learning")
-
-	//declaration of variables in golang
-
-	var x = 10
-	y := 10
-	var a int = 10
-	var b int64
-	b = 32000000000000000
-	var c float64 = 3.14
-	//var d int16 = 10043124321432 //./main.go:16:6: constant 10043124321432 overflows int16
-	//fmt.Println(d)
-
-	fmt.Println(c)
-	fmt.Println(b)
-	fmt.Println(y)
-	fmt.Println(a)
+	var x string = "Hello"
 	fmt.Println(x)
+
+	//to get the length of a string
+	fmt.Println(len(x)) //this will not give you the actual length of the string
+	fmt.Println(utf8.RuneCountInString(x))
+	fmt.Println(utf8.RuneCount([]byte(x)))
+	fmt.Println([]byte(x))
 }
